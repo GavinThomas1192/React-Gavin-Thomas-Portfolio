7257d022b2f1d55195e6b7781e3334cc3a148ae7
GavinThomas1192 <gthomas1192@gmail.com>
2020-01-14 14:12:11 -0800

    2020 updates

--- src/javascripts/utils/clients.js
+++ src/javascripts/utils/clients.js
@@ -1,6 +1,43 @@
 
 const projects = [
   {
+    title: "Software Engineer - Logistics Intelligence - Seattle, WA",
+    description:
+      "Full Stack Software Engineer",
+    slug: "li",
+    stack: "Using: React-Native, React, Node-CQRS, Docker, AWS, and GCP",
+    link: "https://www.logisticsintelligence.com/",
+    github: "https://github.com/AttackPattern",
+    img: require("../../img/facetDev/facetDev2.png"),
+    author: "Gavin Thomas",
+    date: "2018-2020",
+    technicalDescription:
+      `Built from the ground up my team built a full stack solution to the logistics problems facing the oil and gas industry. We provided a logistics solution for wasted time and resources in all aspects of the operation. Our team built a web portal, mobile apps, and analytics portal all disrupting the market tremendously.`,
+    featured: true,
+    secondaryPictures: [
+      require("../../img/li/li1.png"),
+    ]
+  },
+  {
+    title: "Mobile App - Archbright - Seattle, WA",
+    description:
+      "Full Stack Mobile Engineer",
+    slug: "archbright",
+    stack: "Using: React-Native, Node, Docker, and Azure",
+    link: "https://www.archbright.com/page/archbright-mobile",
+    github: "https://github.com/AttackPattern",
+    img: require("../../img/archbright/archbrightTest.png"),
+    author: "Gavin Thomas",
+    date: "2018 - 2019",
+    technicalDescription:
+    `Designed, Engineered, Built, Tested, and Deployed Archbright Mobile was Archbright's first mobile application aiming to make their customers lives easier. My team built both a new mobile app (android and iOS), and an accompanying server built with node. Solving Archbright's unique problems, consisting of needing a new mobile app yet integrating with older existing api's that they currently used via their CRM. Mobile apps can be found from the linked website at the bottom.`,
+    featured: true,
+    secondaryPictures: [
+      require("../../img/archbright/archbright2.png"),
+      require("../../img/archbright/archbrightTest.png"),
+    ]
+  },
+  {
     title: "Software Developer - Facet Development - Seattle, WA",
     description:
       "Full Stack Software Developer",
@@ -122,114 +159,114 @@ const projects = [
       require("../../img/cavness/cavness4.png")
     ]
   },
-  {
-    github: "https://github.com/GavinThomas1192/HackTheNow",
-    stack: "Using: React, Node, Socket.IO, and Microsoft Cognitive Services",
-    img: require("../../img/babble/babble1.png"),
-    title: "Babble",
-    author: "Gavin Thomas & Megan Flood",
-    link: "https://github.com/GavinThomas1192/HackTheNow",
-    description:
-      "This app is a real time speech translator capable of translating over 50 different languages!",
-    date: "2018",
-    technicalDescription:
-      "This project was built over the course of only 9 hours for Ratio/Globant's Hack The Now AI Hackathon hosted on Feb 4, 2018 in Seattle. We used React as our front end, paired with a simple http server using Socket.IO. Simply input which language you are translating to and from then let Babble do the rest.  ",
-    secondaryPictures: [
-      require("../../img/babble/babble2.png"),
-      require("../../img/babble/babble3.png"),
-      require("../../img/babble/babble4.png")
-    ]
-  },
-  {
-    github: "https://github.com/GavinThomas1192/201-Final-Project",
-    stack: "Using: HTML, CSS, and vanilla Javascript only!",
-    img: require("../../img/verve/verve1.png"),
-    title: "Verve",
-    author: "Gavin Thomas",
-    link: "https://gavinthomas1192.github.io/201-Final-Project/",
-    description:
-      "A beautiful, fast, rich environment to keep a digital journal.",
-    date: "2017",
-    technicalDescription:
-      "Vanilla Javascript will never be useless. This project was built with no modern frameworks and had one goal. To be visually pleasing to the user. Create an atmosphere where journaling your emotions was easy.",
-    secondaryPictures: [
-      require("../../img/verve/verve2.png"),
-      require("../../img/verve/verve3.png")
-    ]
-  },
+  // {
+  //   github: "https://github.com/GavinThomas1192/HackTheNow",
+  //   stack: "Using: React, Node, Socket.IO, and Microsoft Cognitive Services",
+  //   img: require("../../img/babble/babble1.png"),
+  //   title: "Babble",
+  //   author: "Gavin Thomas & Megan Flood",
+  //   link: "https://github.com/GavinThomas1192/HackTheNow",
+  //   description:
+  //     "This app is a real time speech translator capable of translating over 50 different languages!",
+  //   date: "2018",
+  //   technicalDescription:
+  //     "This project was built over the course of only 9 hours for Ratio/Globant's Hack The Now AI Hackathon hosted on Feb 4, 2018 in Seattle. We used React as our front end, paired with a simple http server using Socket.IO. Simply input which language you are translating to and from then let Babble do the rest.  ",
+  //   secondaryPictures: [
+  //     require("../../img/babble/babble2.png"),
+  //     require("../../img/babble/babble3.png"),
+  //     require("../../img/babble/babble4.png")
+  //   ]
+  // },
+  // {
+  //   github: "https://github.com/GavinThomas1192/201-Final-Project",
+  //   stack: "Using: HTML, CSS, and vanilla Javascript only!",
+  //   img: require("../../img/verve/verve1.png"),
+  //   title: "Verve",
+  //   author: "Gavin Thomas",
+  //   link: "https://gavinthomas1192.github.io/201-Final-Project/",
+  //   description:
+  //     "A beautiful, fast, rich environment to keep a digital journal.",
+  //   date: "2017",
+  //   technicalDescription:
+  //     "Vanilla Javascript will never be useless. This project was built with no modern frameworks and had one goal. To be visually pleasing to the user. Create an atmosphere where journaling your emotions was easy.",
+  //   secondaryPictures: [
+  //     require("../../img/verve/verve2.png"),
+  //     require("../../img/verve/verve3.png")
+  //   ]
+  // },
 
-  {
-    github: "https://github.com/arn1313/kritter-frontend",
-    stack: "Using: React, MongoDB, AWS S3, and Webpack",
-    img: require("../../img/kritter/kritter4.png"),
-    title: "Kritter",
-    author: "Gavin Thomas",
-    link: "http://kritter.club",
-    description:
-      "In a world of negativity Kritter gives animals lovers a place to have a laugh.",
-    featured: true,
-    date: "2017",
-    technicalDescription:
-      "This is a full stack application written by myself and three others. The back end is powered by Node.js using MongoDB. We paired it with a fast React Front end communicating with AWS for cloud photo storage. First time users, please be patient with Heroku.",
-    secondaryPictures: [
-      require("../../img/kritter/kritter1.png"),
-      require("../../img/kritter/kritter2.png"),
-      require("../../img/kritter/kritter3.png")
-    ]
-  },
-  {
-    github: "https://github.com/GavinThomas1192/firstChromeExt",
-    stack: "Using: React, ChromeDevTools, Webpack, and Material IO",
-    img: require("../../img/quicknote/quicknote1.png"),
-    title: "QuickNote",
-    author: "Gavin Thomas",
-    link:
-      "https://chrome.google.com/webstore/detail/quicknote/dcpdoelecfeoaagkleajgaeedkdaibad",
-    description:
-      "A fast, lightweight, and easy to use chrome extension note taking app.",
-    date: "2017",
-    technicalDescription:
-      "Yes, you can build a Chrome Extension with React! This project demonstrated how awesome React can be when paired with Lightweight delivery methods such as Chrome Extensions.",
-    secondaryPictures: [
-      require("../../img/quicknote/quicknote1.png"),
-      require("../../img/quicknote/quicknote2.png"),
-      require("../../img/quicknote/quicknote3.png")
-    ]
-  },
-  {
-    github: "https://github.com/Lonewalker72/API_Supply",
-    stack: "Using: MongoDB, AWS, Node.js, and React",
-    img: require("../../img/apisupply/api1.png"),
-    title: "API-Supply",
-    author: "Gavin Thomas",
-    link: "https://gavinthomas1192.github.io/API-SupplyFront/",
-    description:
-      "API-Supply provides developers a mainstreamed source to get important information about popular APIs for their project",
-    date: "2017",
-    technicalDescription:
-      "This database was built on Node.js and MongoDB. The idea was to create a database of API's with important collective information on each of them. We did the work of vetting, figuring out keys, pricing, and limits. Then we packaged it up in a nice RESTful API to be consumed",
-    secondaryPictures: [
-      require("../../img/apisupply/api1.png"),
-      require("../../img/apisupply/api2.png"),
-      require("../../img/apisupply/api3.png")
-    ]
-  },
-  {
-    github: "https://github.com/GavinThomas1192/InstyGrammy-Front",
-    stack: "Using: React, MongoDB, AWS S3, Node.js, and React-Bootstrap",
-    img: require("../../img/moreWork/insty.png"),
-    title: "Instygrammy",
-    author: "Gavin Thomas",
-    link: "http://instygrammy.com",
-    description:
-      "A fast, easy to use photo sharing web application designed for use within families.",
-    date: "2017",
-    secondaryPictures: [
-      require("../../img/moreWork/int1.png"),
-      require("../../img/moreWork/int2.png"),
-      require("../../img/moreWork/int3.png")
-    ]
-  },
+  // {
+  //   github: "https://github.com/arn1313/kritter-frontend",
+  //   stack: "Using: React, MongoDB, AWS S3, and Webpack",
+  //   img: require("../../img/kritter/kritter4.png"),
+  //   title: "Kritter",
+  //   author: "Gavin Thomas",
+  //   link: "http://kritter.club",
+  //   description:
+  //     "In a world of negativity Kritter gives animals lovers a place to have a laugh.",
+  //   featured: true,
+  //   date: "2017",
+  //   technicalDescription:
+  //     "This is a full stack application written by myself and three others. The back end is powered by Node.js using MongoDB. We paired it with a fast React Front end communicating with AWS for cloud photo storage. First time users, please be patient with Heroku.",
+  //   secondaryPictures: [
+  //     require("../../img/kritter/kritter1.png"),
+  //     require("../../img/kritter/kritter2.png"),
+  //     require("../../img/kritter/kritter3.png")
+  //   ]
+  // },
+  // {
+  //   github: "https://github.com/GavinThomas1192/firstChromeExt",
+  //   stack: "Using: React, ChromeDevTools, Webpack, and Material IO",
+  //   img: require("../../img/quicknote/quicknote1.png"),
+  //   title: "QuickNote",
+  //   author: "Gavin Thomas",
+  //   link:
+  //     "https://chrome.google.com/webstore/detail/quicknote/dcpdoelecfeoaagkleajgaeedkdaibad",
+  //   description:
+  //     "A fast, lightweight, and easy to use chrome extension note taking app.",
+  //   date: "2017",
+  //   technicalDescription:
+  //     "Yes, you can build a Chrome Extension with React! This project demonstrated how awesome React can be when paired with Lightweight delivery methods such as Chrome Extensions.",
+  //   secondaryPictures: [
+  //     require("../../img/quicknote/quicknote1.png"),
+  //     require("../../img/quicknote/quicknote2.png"),
+  //     require("../../img/quicknote/quicknote3.png")
+  //   ]
+  // },
+  // {
+  //   github: "https://github.com/Lonewalker72/API_Supply",
+  //   stack: "Using: MongoDB, AWS, Node.js, and React",
+  //   img: require("../../img/apisupply/api1.png"),
+  //   title: "API-Supply",
+  //   author: "Gavin Thomas",
+  //   link: "https://gavinthomas1192.github.io/API-SupplyFront/",
+  //   description:
+  //     "API-Supply provides developers a mainstreamed source to get important information about popular APIs for their project",
+  //   date: "2017",
+  //   technicalDescription:
+  //     "This database was built on Node.js and MongoDB. The idea was to create a database of API's with important collective information on each of them. We did the work of vetting, figuring out keys, pricing, and limits. Then we packaged it up in a nice RESTful API to be consumed",
+  //   secondaryPictures: [
+  //     require("../../img/apisupply/api1.png"),
+  //     require("../../img/apisupply/api2.png"),
+  //     require("../../img/apisupply/api3.png")
+  //   ]
+  // },
+  // {
+  //   github: "https://github.com/GavinThomas1192/InstyGrammy-Front",
+  //   stack: "Using: React, MongoDB, AWS S3, Node.js, and React-Bootstrap",
+  //   img: require("../../img/moreWork/insty.png"),
+  //   title: "Instygrammy",
+  //   author: "Gavin Thomas",
+  //   link: "http://instygrammy.com",
+  //   description:
+  //     "A fast, easy to use photo sharing web application designed for use within families.",
+  //   date: "2017",
+  //   secondaryPictures: [
+  //     require("../../img/moreWork/int1.png"),
+  //     require("../../img/moreWork/int2.png"),
+  //     require("../../img/moreWork/int3.png")
+  //   ]
+  // },
   {
     github: "https://github.com/GavinThomas1192",
     stack: "Using: Javascript, jQuery, HTML, and CSS",
@@ -269,22 +306,22 @@ const projects = [
       require("../../img/meekanic/meekanic7.png")
     ]
   },
-  {
-    github: "https://github.com/esack7/301-Final_Project",
-    stack: "Using: jQuery, Express, and Node.js",
-    img: require("../../img/pickaflick/pick1.png"),
-    title: "Pick-A-Flick",
-    author: "Gavin Thomas",
-    link: "https://pic-a-flic.herokuapp.com/",
-    description: "Never argue over what move to pick again.",
-    date: "2017",
-    technicalDescription:
-      "Powered by Node.js this Web Application was built with old school jQuery and CSS. It pull data from external, open, API's like IMDB and OMDB.",
-    secondaryPictures: [
-      require("../../img/pickaflick/pick1.png"),
-      require("../../img/pickaflick/pick2.png")
-    ]
-  }
+  // {
+  //   github: "https://github.com/esack7/301-Final_Project",
+  //   stack: "Using: jQuery, Express, and Node.js",
+  //   img: require("../../img/pickaflick/pick1.png"),
+  //   title: "Pick-A-Flick",
+  //   author: "Gavin Thomas",
+  //   link: "https://pic-a-flic.herokuapp.com/",
+  //   description: "Never argue over what move to pick again.",
+  //   date: "2017",
+  //   technicalDescription:
+  //     "Powered by Node.js this Web Application was built with old school jQuery and CSS. It pull data from external, open, API's like IMDB and OMDB.",
+  //   secondaryPictures: [
+  //     require("../../img/pickaflick/pick1.png"),
+  //     require("../../img/pickaflick/pick2.png")
+  //   ]
+  // }
 ];
 
 export default projects;
